b17a1c8c8b11ff6d74635fdc36b8f2e5e163ec6c
Atanas Dimitrov <hi.atanasdim@gmail.com>
2023-03-31 17:17:30 +0100

    Feature | Hope UI (#1)
    * Create new Header and update Search with HopeUI components
    
    * Add loading spinner on Search

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
+import { Container, HopeProvider } from "@hope-ui/solid";
 import type { Component } from "solid-js";
 
 import styles from "./App.module.scss";
-
+import Header from "./components/header/Header";
 import Search from "./components/search/Search";
 
 const App: Component = () => {
   return (
-    <div class={styles.app}>
-      <header class={styles.header}>
-        <h1>Groceries</h1>
-      </header>
-      <Search />
-    </div>
+    <HopeProvider>
+      <Container class={styles.app}>
+        <Header />
+        <Search />
+      </Container>
+    </HopeProvider>
   );
 };
 
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,41 +1,79 @@
+import {
+  AspectRatio,
+  Box,
+  CircularProgress,
+  Grid,
+  GridItem,
+  Input,
+  SimpleGrid,
+  Text,
+} from "@hope-ui/solid";
 import { Component, createSignal } from "solid-js";
+
 import { searchGroceries } from "../../services/groceries-api";
-import styles from "./Search.module.scss";
 
 const Search: Component = () => {
   const [searchTerm, setSearchTerm] = createSignal<string>("");
   const [searchResults, setSearchResults] = createSignal<any[]>([]);
+  const [isLoading, setIsLoading] = createSignal(false);
 
   const onSubmit = (e: SubmitEvent) => {
     e.preventDefault();
+    setIsLoading(true);
     searchGroceries(searchTerm()).then((data) => {
       console.log({ data });
       setSearchResults(data);
+      setIsLoading(false);
     });
   };
 
   return (
     <>
-      <form onSubmit={onSubmit} class={styles.form}>
-        <input
+      <Box as="form" onSubmit={onSubmit} css={{ width: "100%" }}>
+        <Input
           type="text"
           placeholder="Search"
-          class={styles.search}
           onChange={(e) => setSearchTerm((e.target as HTMLInputElement).value)}
         />
-      </form>
-      <div class={styles.results}>
+      </Box>
+      {isLoading() && (
+        <CircularProgress
+          css={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%,-50%)",
+          }}
+        />
+      )}
+      <SimpleGrid
+        columns={{ "@initial": 2, "@md": 3, "@lg": 4, "@xl": 5 }}
+        gap="$6"
+        css={{ width: "100%", mt: 16 }}
+      >
         {searchResults().map((result, index) => {
           return (
-            <div class={styles.card}>
-              <img src={result.food.image} alt={result.food.label} />
-              <span>
-                {index}. {result.food.label}
-              </span>
-            </div>
+            <GridItem
+              css={{
+                border: "$neutral6 solid 1px",
+                borderRadius: "$lg",
+                overflow: "hidden",
+              }}
+            >
+              <AspectRatio ratio={1}>
+                <img src={result.food.image} alt={result.food.label} />
+              </AspectRatio>
+              <Text
+                as="span"
+                size="lg"
+                css={{ px: 8, py: 4, display: "block" }}
+              >
+                {result.food.label}
+              </Text>
+            </GridItem>
           );
         })}
-      </div>
+      </SimpleGrid>
     </>
   );
 };
